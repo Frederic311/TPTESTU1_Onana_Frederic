fc2b668851d06359a65a7f4f15e1db0a4dfe2a03
Frederic311 <onanafrederic31@gmail.com>
2024-10-25 09:47:44 +0100

    factorial and positive number ok

--- TPTESTU1_Onana/tptestu1_onana/src/main/java/com/example/Main.java
+++ TPTESTU1_Onana/tptestu1_onana/src/main/java/com/example/Main.java
@@ -1,7 +1,11 @@
 package com.example;
 
+import com.stjean.operation.OperationMathematique;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+        int nombrePositif = 5;
+        System.out.println("Est positif(" + nombrePositif + ") : " + OperationMathematique.estPositif(nombrePositif));
     }
 }
--- TPTESTU1_Onana/tptestu1_onana/src/main/java/com/stjean/operation/OperationMathematique.java
+++ TPTESTU1_Onana/tptestu1_onana/src/main/java/com/stjean/operation/OperationMathematique.java
@@ -2,4 +2,19 @@ package com.stjean.operation;
 
 public class OperationMathematique {
 
+    public static boolean estPositif(int nombre) {
+        return nombre > 0;
+    }
+
+    public static int factorial(int number) {
+        if (number < 0) {
+            throw new IllegalArgumentException("Number cannot be negative.");
+        }
+
+        if (number == 0 || number == 1) {
+            return 1;
+        }
+
+        return number * factorial(number - 1);
+    }
 }
