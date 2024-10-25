b467cf885e47f4a89e3ce1232b32018ea250eff4
Frederic311 <onanafrederic31@gmail.com>
2024-10-25 10:32:58 +0100

    ok

--- TPTESTU1_Onana/tptestu1_onana/src/main/java/com/example/Main.java
+++ TPTESTU1_Onana/tptestu1_onana/src/main/java/com/example/Main.java
@@ -7,5 +7,6 @@ public class Main {
         System.out.println("Hello world!");
         int nombrePositif = 5;
         System.out.println("Est positif(" + nombrePositif + ") : " + OperationMathematique.estPositif(nombrePositif));
+
     }
 }
